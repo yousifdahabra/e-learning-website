1fb9574c0b9017c2ccb6677b7e1a4381ee371506
YousifDeveloper <yousif.dahabra.97@gmail.com>
2024-11-19 00:49:31 +0200

    Update Login Page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -25,59 +25,87 @@ const Login = () => {
     <>
     
     <Header/>
-    
-    
+    <div class="flex main-container">
+        <div class="nav-container">
+            <nav class="flex flex-direction-column justify-content-space-between second-background-color nav">
+                <div class="nav-options">
+                    <a href="#" class="nav-option active">
+                        <i class="fa fa-tachometer" aria-hidden="true"></i>
+                        <h3> Login</h3>
+                    </a>
+                </div>
+            </nav>
+        </div>
+    </div>
+    <div id="form_model" class="login-model">
+        <div class="login-modal-content flex flex-direction-column align-items-center justify-content-center flex-wrap">
+            <div class="form flex flex-direction-column">
+                <div class=" flex flex-wrap-nowrap align-items-start  form-group">
+                  {error && <label class="alert" id="message_alert">{error}</label>}
+
+                    
+                </div>
+
+                <div class=" flex flex-wrap-nowrap align-items-start  form-group">
+                    <label for="User Name">User Name</label>
+                    <input id="username" type="text"
+                      onChange={(e) => {
+                        setLoginForm({
+                          ...loginForm,
+                          username: e.target.value,
+                        });
+                    
+                    
+                      }}
+                    
+                    
+                    />
+                </div>
+                <div class=" flex flex-wrap-nowrap align-items-start  form-group">
+                    <label for="Password">Password</label>
+                    <input id="password"  type="password"
+                      onChange={(e) => {
+                        setLoginForm({
+                          ...loginForm,
+                          password: e.target.value,
+                        });
+                      }}
+                    
+                    
+                    />
+                </div>
+
+                <div class=" flex flex-wrap-nowrap align-items-start  form-group">
+                    <button id="submit_login_form" class="view" type="button"
+                      onClick={() => {
+                        setError("");
+                    
+                        const data = new FormData();
+                    
+                        data.append("username", loginForm.username);
+                        data.append("password", loginForm.password);
+                    
+                        axios
+                          .post("http://localhost/ecommerse_website/server/login.php", data)
+                          .then((res) => {
+                            localStorage.setItem("userId", res.data.user.id);
+                    
+                            navigate("/users");
+                          })
+                          .catch((error) => {
+                            setError(error.response.data.status);
+                          });
+                      }}
+                    
+                    
+                    >Submit</button>
+                </div>
+            </div>
+        </div>
+    </div>
+ 
+
     </>
-    // <div>
-
-    //   <h1>Login</h1>
-    //   <input
-    //     type="text"
-    //     placeholder="Username"
-    //     onChange={(e) => {
-    //       setLoginForm({
-    //         ...loginForm,
-    //         username: e.target.value,
-    //       });
-
-
-    //     }}
-    //   />
-    //   <input
-    //     type="password"
-    //     placeholder="Password"
-    //     onChange={(e) => {
-    //       setLoginForm({
-    //         ...loginForm,
-    //         password: e.target.value,
-    //       });
-    //     }}
-    //   />
-    //   <button
-    //     onClick={() => {
-    //       setError("");
-
-    //       const data = new FormData();
-
-    //       data.append("username", loginForm.username);
-    //       data.append("password", loginForm.password);
-
-    //       axios
-    //         .post("http://localhost/ecommerse_website/server/login.php", data)
-    //         .then((res) => {
-    //           localStorage.setItem("userId", res.data.user.id);
-
-    //           navigate("/users");
-    //         })
-    //         .catch((error) => {
-    //           setError(error.response.data.status);
-    //         });
-    //     }}
-    //   >
-    //     Login
-    //   </button>
-    //   {error && <p>{error}</p>}
-    // </div>
   );
 };
 
