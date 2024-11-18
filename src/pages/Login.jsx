7c484c5f79a58caf4a0d40bd19b51150869713d7
YousifDeveloper <yousif.dahabra.97@gmail.com>
2024-11-19 00:30:42 +0200

    Add base styles and dashboard

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import '../styles/pages/login.css';
 
 const Login = () => {
   const navigate = useNavigate();
