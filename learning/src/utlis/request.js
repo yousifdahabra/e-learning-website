a97667c274064ad150d30cafb49c615cd155a6f2
YousifDeveloper <yousif.dahabra.97@gmail.com>
2024-11-19 22:20:11 +0200

    Update request set the header dynamic for features such as the header will change only when passing the files

--- learning/src/utlis/request.js
+++ learning/src/utlis/request.js
@@ -2,20 +2,21 @@ import axios from 'axios'
 
 axios.defaults.baseURL = "http://localhost/learning/back/";
 
-export const requestAPI =  async({route,method = 'GET' ,body}) =>{
+export const requestAPI =  async({route,method = 'GET' ,body,header='application/json'}) =>{
     //route like api file php
     //method post/get
     //body is data
     //ON PHP SET Authorization 
+    //multipart/form-data for image hundel in side back using $_POST
     try{
         const respons = await axios.request({
             url:`${route}.php`,
             method,
             data:body,
             headers:{
-                'Content-Type': 'application/x-www-form-urlencoded',
+                'Content-Type': header,
                 // "Content-Type":"application/json",
-                // Authorization:localStorage.token,
+                Authorization:localStorage.token,
             }
         });
         return respons.data;
