a88ececc971a4e087b0b9206a054d846050d483d
YousifDeveloper <yousif.dahabra.97@gmail.com>
2024-11-19 04:51:51 +0200

    Update AdminDashboard.jsx , set report header

---
+++ learning/src/components/admin/ReportHeader.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+const ReportHeader =() => {
+
+    return (
+    <div className="report-header">
+        <h1 className="recent-Articles">Tracking Report</h1>
+        <div className="report-btn flex flex-wrap align-content-center justify-content-center align-items-center">
+            <button id="transaction_btn" className="view"   >Add</button>
+        </div>
+
+    </div>
+
+    )
+}
+
+
+export default ReportHeader
--- learning/src/pages/AdminDashboard.jsx
+++ learning/src/pages/AdminDashboard.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Header from '../components/base/Header'
 import AdminNavContainer from '../components/admin/AdminNavContainer'
+import ReportHeader from '../components/admin/ReportHeader'
 import Boxes from '../components/admin/Boxes'
 import axios from "axios";
 
@@ -9,18 +10,30 @@ import axios from "axios";
 
 const AdminDashboard = () =>{
     return (
-        <>
-        <Header/>
+      <>
+        <Header />
         <div className="flex main-container">
+          <AdminNavContainer />
+          <div className="main">
+            <Boxes />
+            <div className="report-container">
+            
+              <div className="report-header">
+                <h1 className="recent-Articles">Users Report</h1>
+                <div className="report-btn flex flex-wrap align-content-center justify-content-center align-items-center">
+                  <button id="transaction_btn" className="view">
+                    Add
+                  </button>
+                </div>
+              </div>
 
-        <AdminNavContainer/>
-        <div className="main">
-        <Boxes/>
 
+
+
+            </div>
+          </div>
         </div>
-
-        </div>
-        </>
+      </>
     );
 }
 
