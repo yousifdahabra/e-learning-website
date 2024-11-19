0e85296cdc7d7577049d5af987a6bc8cbbc5ec56
YousifDeveloper <yousif.dahabra.97@gmail.com>
2024-11-20 00:17:27 +0200

    Fix bug by set header to application/x-www-form-urlencoded

--- learning/src/utlis/request.js
+++ learning/src/utlis/request.js
@@ -2,12 +2,11 @@ import axios from 'axios'
 
 axios.defaults.baseURL = "http://localhost/learning/back/";
 
-export const requestAPI =  async({route,method = 'GET' ,body,header='application/json'}) =>{
+export const requestAPI =  async({route,method = 'GET' ,body,header='application/x-www-form-urlencoded'}) =>{
     //route like api file php
     //method post/get
     //body is data
     //ON PHP SET Authorization 
-    //multipart/form-data for image hundel in side back using $_POST
     try{
         const respons = await axios.request({
             url:`${route}.php`,
