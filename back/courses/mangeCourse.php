e0c43af4570d8dbeee0d48cf445c426a7c3145f5
YousifDeveloper <yousif.dahabra.97@gmail.com>
2024-11-20 00:52:46 +0200

    Update mangeCourse add authorization layer

--- back/courses/mangeCourse.php
+++ back/courses/mangeCourse.php
@@ -8,66 +8,74 @@ try {
     $token = $headers['Authorization'];
     $key = new Key($secret_key,"HS256");
     $payload = JWT::decode($token,$key);
+    if( $payload->role == 'admin'){
+        if(isset($_POST['course_name']) && isset($_POST['user_id']) && isset($_POST['description'])  ){
+            if(!empty($_POST['course_name']) && !empty($_POST['user_id']) && !empty($_POST['description']) ){
+                if($_POST['course_id'] == 0){
+                    $insert = $db->modify_query([
+                        "query"=>"Insert  into courses_tbl (course_name,user_id,description) values(?,?,?)",
+                        "types" => "sss",
+                        "params" => [$_POST['course_name'],$_POST['user_id'],$_POST['description']]
+                    ]);
+                    if($insert){
+                        $response = [
+                            "state" => "1",
+                            "message" => "Insert Course",
 
-    if(isset($_POST['course_name']) && isset($_POST['user_id']) && isset($_POST['description'])  ){
-        if(!empty($_POST['course_name']) && !empty($_POST['user_id']) && !empty($_POST['description']) ){
-            if($_POST['course_id'] == 0){
-                $insert = $db->modify_query([
-                    "query"=>"Insert  into courses_tbl (course_name,user_id,description) values(?,?,?)",
-                    "types" => "sss",
-                    "params" => [$_POST['course_name'],$_POST['user_id'],$_POST['description']]
-                ]);
-                if($insert){
-                    $response = [
-                        "state" => "1",
-                        "message" => "Insert Course",
-                    ];
+                        ];
+                    }else{
+                        $response = [
+                            "state" => "0",
+                            "message" => "Not Insert Course",
+                        ];
+                    }
                 }else{
-                    $response = [
-                        "state" => "0",
-                        "message" => "Not Insert Course",
-                    ];
+                    $update = $db->modify_query([
+                        "query"=>"update courses_tbl set course_name = ? ,user_id = ?,description = ? where course_id = ?",
+                        "types" => "ssss",
+                        "params" => [$_POST['course_name'],$_POST['user_id'],$_POST['description'],$_POST['course_id']]
+                    ]);
+                    if($update){
+                        $response = [
+                            "state" => "1",
+                            "message" => "Update Course",
+                        ];
+                    }else{
+                        $response = [
+                            "state" => "0",
+                            "message" => "Not update ",
+                        ];
+                    }
                 }
+                echo json_encode($response)  ;
+
             }else{
-                $update = $db->modify_query([
-                    "query"=>"update courses_tbl set course_name = ? ,user_id = ?,description = ? where course_id = ?",
-                    "types" => "ssss",
-                    "params" => [$_POST['course_name'],$_POST['user_id'],$_POST['description'],$_POST['course_id']]
-                ]);
-                if($update){
-                    $response = [
-                        "state" => "1",
-                        "message" => "Update Course",
-                    ];
-                }else{
-                    $response = [
-                        "state" => "0",
-                        "message" => "Not update ",
-                    ];
-                }
+                $response = [
+                    "state" => "0",
+                    "message" => "fill all data",
+                ];
+                echo json_encode($response)  ;
+        
             }
-            echo json_encode($response)  ;
-
-        }else{
-            $response = [
-                "state" => "0",
-                "message" => "fill all data",
-            ];
-            echo json_encode($response)  ;
-    
+            
         }
         
-    }
-    
-    if(!isset($_POST['course_name']) || !isset($_POST['user_id']) || !isset($_POST['description'])  ){
-        http_response_code(401);
+        if(!isset($_POST['course_name']) || !isset($_POST['user_id']) || !isset($_POST['description'])  ){
+            http_response_code(401);
+            $response = [
+                "states" => "0",
+                "message" => "You are not authorized to be here",
+            ];
+            echo  json_encode($response)  ;        
+        }
+    }else{
         $response = [
-            "states" => "0",
-            "message" => "You are not authorized to be here",
+            "state" => "2",
+            "message" => "not authrize to insert or update course",
         ];
-        echo  json_encode($response)  ;        
-    }
+        echo json_encode($response)  ;
     
+    }
 
     
 
