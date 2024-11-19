b3015bef4dec5c3edc9db3f4bb651ccb674a7db2
YousifDeveloper <yousif.dahabra.97@gmail.com>
2024-11-20 01:00:54 +0200

    Update Courses.php add authorization layer

--- back/courses/Courses.php
+++ back/courses/Courses.php
@@ -1,16 +1,43 @@
 <?php 
 include "../connection.php";
+use Firebase\JWT\Key;
+use Firebase\JWT\JWT;
+try {
 
+$headers = getallheaders();
+$token = $headers['Authorization'];
+$key = new Key($secret_key,"HS256");
+$payload = JWT::decode($token,$key);
+if($payload->role == "admin"){
+    $get_courses = $db->select_query([
+        "query"=>"Select * from courses_tbl   ",
+        "types" => "",
+        "params" => []
+    ]);
+    $response = [
+        "states" => "1",
+        "messages" => "get courses ",
+        "result" => $get_courses
+    ];
+    echo  json_encode($response)  ;
+}else if($payload->role == "instructor"){
+    $get_courses = $db->select_query([
+        "query"=>"Select * from courses_tbl where user_id= ?  ",
+        "types" => "i",
+        "params" => [$payload->user_id]
+    ]);
+    $response = [
+        "states" => "1",
+        "messages" => "get courses ",
+        "result" => $get_courses
+    ];
+    echo  json_encode($response)  ;
+}
+} catch (\Throwable $th) {
 
-$get_courses = $db->select_query([
-    "query"=>"Select * from courses_tbl   ",
-    "types" => "",
-    "params" => []
-]);
-$response = [
-    "states" => "1",
-    "messages" => "get courses ",
-    "result" => $get_courses
-];
-echo  json_encode($response)  ;
-
+    $response = [
+        "state" => "2",
+        "message" => "invalid token - ".$th,
+    ];
+    echo json_encode($response)  ;
+}
