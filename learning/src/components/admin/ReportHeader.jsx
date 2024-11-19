0ba2d2977db1336ff735a479b00f0814dc89e0fb
YousifDeveloper <yousif.dahabra.97@gmail.com>
2024-11-19 22:16:45 +0200

    Update ReportHeader title

--- learning/src/components/admin/ReportHeader.jsx
+++ learning/src/components/admin/ReportHeader.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-const ReportHeader =() => {
+const ReportHeader =({triggerAdd}) => {
 
     return (
     <div className="report-header">
-        <h1 className="recent-Articles">Tracking Report</h1>
+        <h1 className="recent-Articles">User Report</h1>
         <div className="report-btn flex flex-wrap align-content-center justify-content-center align-items-center">
-            <button id="transaction_btn" className="view"   >Add</button>
+            <button  className="view" onClick={triggerAdd} >Add</button>
         </div>
 
     </div>
