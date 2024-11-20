da9077a779006f6c6b1122e1cfc36ded0eab399f
YousifDeveloper <yousif.dahabra.97@gmail.com>
2024-11-20 19:13:00 +0200

    Update Boxes component to be dynamic

--- learning/src/components/admin/Boxes.jsx
+++ learning/src/components/admin/Boxes.jsx
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Boxes = () =>{
+    const [getReport,setReport]= useState({
+        'students':0,
+        'instructors':0,
+        'courses':0,
+    });
+
     return (
         <div className="flex justify-content-evenly align-items-center justify-content-space-around flex-wrap box-container ">
         <div className="flex align-items-center justify-content-space-between box ">
             <div className="topic">
-                <h2 id="incomes" className="topic-heading ">0</h2>
+                <h2 id="incomes" className="topic-heading ">{getReport.students}</h2>
                 <h2 className="topic-title">Students</h2>
             </div>
             <i className="fa fa-plus" aria-hidden="true"></i>
@@ -13,7 +19,7 @@ const Boxes = () =>{
 
         <div className="flex align-items-center justify-content-space-between box ">
             <div className="topic">
-                <h2 className="topic-heading">0</h2>
+                <h2 className="topic-heading">{getReport.instructors}</h2>
                 <h2 className="topic-title">Instructors</h2>
             </div>
             <i className="fa fa-minus" aria-hidden="true"></i>
@@ -21,7 +27,7 @@ const Boxes = () =>{
 
         <div className="flex align-items-center justify-content-space-between box ">
             <div className="topic">
-                <h2   className="topic-heading">0</h2>
+                <h2   className="topic-heading">{getReport.courses}</h2>
                 <h2 className="topic-title">Courses</h2>
             </div>
             <i className="fa fa-balance-scale" aria-hidden="true"></i>
