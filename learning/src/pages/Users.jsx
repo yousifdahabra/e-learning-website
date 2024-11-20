c782d3351da378a7348f1f6d0f35842da0f9b8ae
YousifDeveloper <yousif.dahabra.97@gmail.com>
2024-11-21 01:16:19 +0200

    Update Users page by adding ReportBody components

--- learning/src/pages/Users.jsx
+++ learning/src/pages/Users.jsx
@@ -2,14 +2,22 @@ import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Header from '../components/base/Header'
 import NavContainer from "../components/users/NavContainer";
+import ReportBody from "../components/users/ReportBody";
 
 const Users = () =>{
 
     return <>
     <Header/>
     <div className="flex main-container">
-
         <NavContainer/>
+        <div className="main">
+        <div className="report-container">
+            <ReportBody/>
+        </div>
+
+        </div>
+
+
     </div>
     </>;
 }
