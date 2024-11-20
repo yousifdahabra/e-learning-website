92168b09124b7d453dc1f1fc1195bb23243ce6d4
YousifDeveloper <yousif.dahabra.97@gmail.com>
2024-11-21 01:20:23 +0200

    Update Courses.php by update query

--- back/courses/Courses.php
+++ back/courses/Courses.php
@@ -23,7 +23,8 @@ if($payload->role == "admin"){
     echo  json_encode($response)  ;
 }else if($payload->role == "instructor"){
     $get_courses = $db->select_query([
-        "query"=>"Select * from courses_tbl where user_id= ?  ",
+        "query"=>"Select * from courses_tbl c
+                  left join users_tbl s on c.user_id = s.user_id where c.user_id= ?  ",
         "types" => "i",
         "params" => [$payload->user_id]
     ]);
