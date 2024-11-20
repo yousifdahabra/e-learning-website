7fef97dfabdb21e9ea5e71d47725af9f2b5c79de
YousifDeveloper <yousif.dahabra.97@gmail.com>
2024-11-21 01:46:56 +0200

    Update Posts by adding a  Navbar

--- learning/src/pages/Posts.jsx
+++ learning/src/pages/Posts.jsx
@@ -1,15 +1,18 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Header from "../components/base/Header";
+import NavContainer from "../components/users/NavContainer";
 const Posts = () =>{
 
     return <>
     <Header/>
     <div className="flex main-container">
-         <div className="main">
-        <div className="report-container">
+        <NavContainer/>
 
-         </div>
+         <div className="main">
+            <div className="report-container">
+
+            </div>
         </div>
     </div>
     </>;
