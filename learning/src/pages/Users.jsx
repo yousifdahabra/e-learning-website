20c016fa958132abd25aa069520fbef32ce75cff
YousifDeveloper <yousif.dahabra.97@gmail.com>
2024-11-21 00:29:42 +0200

    Add Users page for students and instructors

---
+++ learning/src/pages/Users.jsx
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Header from '../components/base/Header'
+
+const Users = () =>{
+
+    return <>
+    <Header/>
+    </>;
+}
+
+
+
+export default Users;
