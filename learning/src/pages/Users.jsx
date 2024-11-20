bcd1c08208b9d5194c7fbaa9b3f4b8f1b9197905
YousifDeveloper <yousif.dahabra.97@gmail.com>
2024-11-21 00:57:37 +0200

    Add  Navbar components in User

--- learning/src/pages/Users.jsx
+++ learning/src/pages/Users.jsx
@@ -1,11 +1,16 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Header from '../components/base/Header'
+import NavContainer from "../components/users/NavContainer";
 
 const Users = () =>{
 
     return <>
     <Header/>
+    <div className="flex main-container">
+
+        <NavContainer/>
+    </div>
     </>;
 }
 
