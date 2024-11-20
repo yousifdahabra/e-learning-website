d588c99253f08d5f56ed59b00253b30ed27201d6
YousifDeveloper <yousif.dahabra.97@gmail.com>
2024-11-21 01:41:45 +0200

    Add New Posts page for post announcements and assignments

---
+++ learning/src/pages/Posts.jsx
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Header from "../components/base/Header";
+const Posts = () =>{
+
+    return <>
+    <Header/>
+    <div className="flex main-container">
+         <div className="main">
+        <div className="report-container">
+
+         </div>
+        </div>
+    </div>
+    </>;
+}
+
+
+
+export default Posts;
