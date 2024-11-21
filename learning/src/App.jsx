43220ee39f43f493b7c42469821d6785cd10d099
YousifDeveloper <yousif.dahabra.97@gmail.com>
2024-11-21 13:03:32 +0200

    Update App.jsx by adding usersContext

--- learning/src/App.jsx
+++ learning/src/App.jsx
@@ -1,33 +1,38 @@
-import './styles/App.css';
+import "./styles/App.css";
 import React, { useState } from "react";
 //import {requestAPI} from './utlis/request.js'
 import { Routes, Route, useLocation } from "react-router-dom";
-import Login from './pages/Login';
-import AdminDashboard from './pages/AdminDashboard';
-import Courses from './pages/Courses';
-import './styles/base/color.css';
-import './styles/base/base.css';
-import './styles/base/flex.css';
-import './styles/pages/dashboard.css';
-import Users from './pages/Users';
-import Posts from './pages/Posts';
+import Login from "./pages/Login";
+import AdminDashboard from "./pages/AdminDashboard";
+import Courses from "./pages/Courses";
+import "./styles/base/color.css";
+import "./styles/base/base.css";
+import "./styles/base/flex.css";
+import "./styles/pages/dashboard.css";
+import Users from "./pages/Users";
+import Posts from "./pages/Posts";
+import UsersProvider from "./context/usersContext";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
-  const location = useLocation();
 
-  return ( 
+  return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Login />} />
+      <BrowserRouter>
+        <UsersProvider>
+          <Routes>
+            <Route path="/" element={<Login />} />
 
-        <Route path="/AdminDashboard" element={<AdminDashboard />} />
-        <Route path="/Courses" element={<Courses />} />
+            <Route path="/AdminDashboard" element={<AdminDashboard />} />
+            <Route path="/Courses" element={<Courses />} />
 
-        <Route path="/Users" element={<Users />} />
-        <Route path="/Posts" element={<Posts />} />
+            <Route path="/Users" element={<Users />} />
+            <Route path="/Posts" element={<Posts />} />
 
-        <Route path="/*" element={<h1>Not Found</h1>} />
-      </Routes>
+            <Route path="/*" element={<h1>Not Found</h1>} />
+          </Routes>
+        </UsersProvider>
+      </BrowserRouter>
     </div>
   );
 }
