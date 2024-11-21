05a21eea750829fab1b4e5362d267a0068acf333
YousifDeveloper <yousif.dahabra.97@gmail.com>
2024-11-21 13:01:10 +0200

    Fix index.js structure

--- learning/src/index.js
+++ learning/src/index.js
@@ -2,14 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 import App from './App';
-import { BrowserRouter } from "react-router-dom";
 
 const rootContainer = document.getElementById("root");
 
 const root = ReactDOM.createRoot(rootContainer);
 root.render(
-  <BrowserRouter>
     <App />
-  </BrowserRouter>
 );
 
