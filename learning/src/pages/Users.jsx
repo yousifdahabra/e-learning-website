860c48fa5078c9821deb8a782e5d269f13293e52
YousifDeveloper <yousif.dahabra.97@gmail.com>
2024-11-21 17:04:49 +0200

    Update Users by adding invite form

--- learning/src/pages/Users.jsx
+++ learning/src/pages/Users.jsx
@@ -3,8 +3,17 @@ import { useNavigate } from "react-router-dom";
 import Header from '../components/base/Header'
 import NavContainer from "../components/users/NavContainer";
 import ReportBody from "../components/users/ReportBody";
-
+import InviteCourseFormModel from "../components/users/posts/InviteCourseFormModel";
 const Users = () =>{
+
+     
+    const [isOpenAdd, setisOpenAdd] = useState(false);
+    const triggerAddForm = () =>{
+        setisOpenAdd((isOpenAdd)=> !isOpenAdd)
+    }
+    const triggerCloseForm = () =>{
+        setisOpenAdd(false)
+    }
 
     return <>
     <Header/>
@@ -12,13 +21,13 @@ const Users = () =>{
         <NavContainer/>
         <div className="main">
         <div className="report-container">
-            <ReportBody/>
+            <ReportBody isOpen={triggerAddForm}/>
         </div>
 
         </div>
-
-
     </div>
+    <InviteCourseFormModel isOpen={isOpenAdd} isClose={triggerCloseForm}   />
+
     </>;
 }
 
